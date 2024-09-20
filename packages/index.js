70a9269037657a304e1985d9da33aee987836619
小方 <2875616188@qq.com>
2024-09-20 19:53:12 +0800

    删除部分无用文件

--- packages/index.js
+++
@@ -1,6 +0,0 @@
-export * from "./buildSvgIcon";
-export * from "./cdnImport";
-export * from "./cdnImport/type";
-export * from "./componentAutoImport";
-export * from "./componentAutoImport/type";
-export * from "./versionUpdate";
